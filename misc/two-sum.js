360f2cf80cb0f97d5cf86a4beea9ee885df4ae4d
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-11 14:22:38 +0530

    suing object

--- misc/two-sum.js
+++ misc/two-sum.js
@@ -8,3 +8,23 @@ const twoSum1 = function (nums, target) {
   }
 };
 // [3,2,4] => 3+2, 3+4, 2+4 => 7, 7, 6
+
+// Using JS Objects
+
+const twoSum = function (nums, target) {
+  var obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    var n = nums[i];
+
+    if (obj[target - n] >= 0) {
+      return [obj[target - n], i];
+    } else {
+      obj[n] = i;
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 26));
+
+// nums = [2,7,11,15], target = 26
