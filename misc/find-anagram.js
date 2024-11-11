270303a73f983a562fc7a90e15259d8c3d0d4d6f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-11 14:56:33 +0530

    anagram 2nd approach

--- misc/find-anagram.js
+++ misc/find-anagram.js
@@ -10,3 +10,22 @@ const isAnagram1 = function (s, t) {
 // anagram => [a,n,a,g,r,a,m] => [a,a,a,m,n,r] => aaamnr
 
 // 2nd approach
+const isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+
+  return true;
+};
+
+console.log(isAnagram("anagram", "nagarm"));
