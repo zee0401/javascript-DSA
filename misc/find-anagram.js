0cf11d989a820020bd977b4bbfabd51578250f67
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-11 14:38:21 +0530

    anagram 1st approach

---
+++ misc/find-anagram.js
@@ -0,0 +1,12 @@
+// 1st approach
+
+const isAnagram1 = function (s, t) {
+  s = s.split("").sort().join("");
+  t = t.split("").sort().join("");
+
+  return s === t;
+};
+
+// anagram => [a,n,a,g,r,a,m] => [a,a,a,m,n,r] => aaamnr
+
+// 2nd approach
