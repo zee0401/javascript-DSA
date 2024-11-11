b3425b9fb33158236c17e07af26b56c1c57e08c4
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-11 18:57:26 +0530

    2nd approach

--- misc/best-time-tbuy-stock.js
+++ misc/best-time-tbuy-stock.js
@@ -14,3 +14,25 @@ function maxProfit1(prices) {
 }
 
 // console.log(maxProfit1([7, 6, 4, 3, 1]));
+
+// Greedy Algorithm
+const maxProfit = function (prices) {
+  let minStockPurchasePrice = prices[0] || 0;
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < minStockPurchasePrice) {
+      minStockPurchasePrice = prices[i];
+    }
+
+    profit = Math.max(profit, prices[i] - minStockPurchasePrice);
+  }
+
+  return profit;
+};
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
+
+// [7, 1, 5, 3, 6, 4]
+// min = 7 => 1
+// profit = 0 => 5-1 = 4 => 6-1 = 5
