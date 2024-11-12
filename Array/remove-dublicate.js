6da39eee04b99759e5c004090a6380ca81a6c7a2
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-12 12:40:12 +0530

    remove dublicate approach 2

--- Array/remove-dublicate.js
+++ Array/remove-dublicate.js
@@ -14,3 +14,22 @@ function removeDuplicates(nums) {
 // Time Complexity - O(n)
 // Space Complexity - O(1)
 // console.log(removeDuplicates([1, 1, 2]));
+
+// Without JS Methods - Two pointer Approach
+function removeDuplicatesNew(nums) {
+  if (nums.length === 0) return 0;
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+}
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+console.log(removeDuplicatesNew([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
