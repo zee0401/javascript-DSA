4b00b9b48bea432416b295382f74223352a5c7ad
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-12 12:42:00 +0530

    kadanes algorithm

--- Array/maximum-subarray.js
+++ Array/maximum-subarray.js
@@ -32,3 +32,28 @@ function maxSubArrayBruteForce(nums) {
 // Space Complexity - O(1)
 
 // console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+// Kadane's Algorithm
+
+// [-2,1,-3,4,-1,2,1,-5,4]
+function maxSubArray(nums) {
+  let sum = 0;
+  let max = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum > max) {
+      max = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+
+  return max;
+}
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+console.log(maxSubArray([5, 4, -1, 7, 8]));
