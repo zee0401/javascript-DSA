17b54814fdbabdd0cd0439b8dd6905a2222c30b9
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-12 12:44:52 +0530

    optimed aproach

--- Array/rotate-array-by-k.js
+++ Array/rotate-array-by-k.js
@@ -21,3 +21,31 @@ function rotateArray(nums, k) {
 
 // Time Complexity - O(n)
 // console.log(rotateArray([1, 2, 3, 4, 5, 6, 7], 3));
+
+// Optimised Approach
+function rotateArrayOptimised(nums, k) {
+  let size = nums.length;
+
+  if (size > k) {
+    k = k % size;
+  }
+
+  reverse(nums, 0, nums.length - 1); // O(n)
+  reverse(nums, 0, k - 1); // O(k)
+  reverse(nums, k, nums.length - 1); // O(n-k)
+
+  return nums;
+}
+
+// Time Complexity -> O(n)
+// Space Complexity -> O(1)
+
+function reverse(nums, left, right) {
+  while (left < right) {
+    const temp = nums[left];
+    nums[left++] = nums[right];
+    nums[right--] = temp;
+  }
+}
+
+console.log(rotateArrayOptimised([-1, -100, 3, 99], 2));
