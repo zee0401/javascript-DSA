db7603fed69364e52e823251db637ab6cb3482ec
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-12 15:15:12 +0530

    added push method

--- Array/customArray.js
+++ Array/customArray.js
@@ -2,5 +2,14 @@ class myArray {
   constructor() {
     (this.length = "0"), (this.data = {});
   }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+  }
 }
 const newArray = new myArray();
+
+newArray.push("apple");
+newArray.push("mango");
+newArray.push("banana");
