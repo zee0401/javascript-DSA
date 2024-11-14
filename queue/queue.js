1080bb9126b9e04fdfab72fb79825bb5fb52fd4e
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-14 12:26:56 +0530

    dequeue

--- queue/queue.js
+++ queue/queue.js
@@ -25,10 +25,28 @@ class Queue {
     this.length++;
     return this;
   }
+
+  dequeue() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let temp = this.first;
+
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 let myQueue = new Queue(0);
 myQueue.enqueue(1);
 myQueue.enqueue(2);
-// myQueue.dequeue();
-// console.log(myQueue);
+myQueue.dequeue();
+console.log(myQueue);
