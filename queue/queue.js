811173cbb6b9597705e4e34cfdfd48721858d497
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-14 12:26:40 +0530

    enqueue

--- queue/queue.js
+++ queue/queue.js
@@ -12,10 +12,23 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 let myQueue = new Queue(0);
-// myQueue.enqueue(1);
-// myQueue.enqueue(2);
+myQueue.enqueue(1);
+myQueue.enqueue(2);
 // myQueue.dequeue();
 // console.log(myQueue);
