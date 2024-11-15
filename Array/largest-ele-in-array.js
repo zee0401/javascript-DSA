026892e646e64e7a6f99f7a60b24e35a15d69c9c
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-11-15 12:36:32 +0530

    largest element in th array

---
+++ Array/largest-ele-in-array.js
@@ -0,0 +1,13 @@
+function findLargestElement(nestedArray) {
+  let largest = nestedArray[0][0];
+
+  for (let arr of nestedArray) {
+    for (let num of arr) {
+      if (num > largest) {
+        largest = num;
+      }
+    }
+  }
+
+  return largest;
+}
