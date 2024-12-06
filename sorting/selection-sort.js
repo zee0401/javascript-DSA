0b494663d90ed8369a2f8b8f6c57fb0a807d303d
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 11:57:35 +0530

    selection sort

---
+++ sorting/selection-sort.js
@@ -0,0 +1,14 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+  }
+  return arr;
+};
+
+console.log(selectionSort(arr)); // -7, -2 , 5, 1000
