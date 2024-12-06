c08c7d839814d0c01fb582e6d4e4f05fa559ac5f
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 12:23:38 +0530

    Convert Number and Object to String

--- string/string.js
+++ string/string.js
@@ -12,3 +12,13 @@ console.log(firstName.charCodeAt(2)); // 115 (Ascii Code)
 console.log(firstName.includes("r")); // false (& if present it return true)
 console.log(firstName.indexOf("i")); // 2 (& if not present it return -1)
 console.log(firstName.lastIndexOf("i")); // 7
+
+// Convert Number and Object to String
+const num = 123;
+console.log(num, num.toString());
+
+const obj = {
+  name: "Vishal",
+  course: "DSA with Vishal",
+};
+console.log(obj, JSON.stringify(obj));
