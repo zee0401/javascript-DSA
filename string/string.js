c03d8e99fe0a2ec0c7b19e32503ad8320c4079b0
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 12:01:43 +0530

    string

---
+++ string/string.js
@@ -0,0 +1,9 @@
+// Length of a String
+
+let firstName = "Vaishali";
+console.log(firstName.length);
+
+// Access String Element
+console.log(firstName.charAt(2)); // i
+console.log(firstName[2]); // i
+console.log(firstName.charCodeAt(2)); // 115 (Ascii Code)
