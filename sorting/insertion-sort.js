a6b0fa82c2a9341e83a6591b580241c664ee291d
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 11:58:41 +0530

    insertion-sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,14 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+};
+
+console.log(insertionSort(arr)); // -7, -2 , 5, 1000
