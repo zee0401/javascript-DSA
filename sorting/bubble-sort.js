4c51d45b9146c700c4f7728bd9ea7c816b7b4f4d
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 11:56:46 +0530

    bubble-sort

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,15 @@
+const bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+
+console.log(bubbleSort(arr)); // -7, -2 , 5, 1000
