2c39cb19a79c1d0c7c80b7e9ec1f8f20e73bbc5c
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 12:28:16 +0530

    Replace Substring

--- string/string.js
+++ string/string.js
@@ -3,22 +3,35 @@
 let firstName = "Vaishali";
 console.log(firstName.length);
 
+// -----------------------------------------
+
 // Access String Element
 console.log(firstName.charAt(2)); // i
 console.log(firstName[2]); // i
 console.log(firstName.charCodeAt(2)); // 115 (Ascii Code)
+
+// -----------------------------------
 
 // Check Presence of Character
 console.log(firstName.includes("r")); // false (& if present it return true)
 console.log(firstName.indexOf("i")); // 2 (& if not present it return -1)
 console.log(firstName.lastIndexOf("i")); // 7
 
+// -------------------------------------------------
+
 // Convert Number and Object to String
 const num = 123;
 console.log(num, num.toString());
 
 const obj = {
-  name: "Vishal",
-  course: "DSA with Vishal",
+  name: "Zeshan",
+  course: "DSA with Zeshan",
 };
 console.log(obj, JSON.stringify(obj));
+
+// ----------------------------------------
+
+// Replace Substring
+const str = "Zeshan is Best Frontend Developer. Aman is Best Developer. ";
+console.log(str.replace("Zeshan", "Aman")); // "Zeshan is Best Frontend Developer. Aman is Best Developer. "
+console.log(str.replaceAll("Zeshan", "Aman")); // "Zeshan is Best Frontend Developer. Zeshan is Best Developer. "
