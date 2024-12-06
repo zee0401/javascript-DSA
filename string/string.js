eb06682579d814ef3847baf3f3a6afd58a8e3047
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-06 12:02:02 +0530

    Check Presence of Character

--- string/string.js
+++ string/string.js
@@ -7,3 +7,8 @@ console.log(firstName.length);
 console.log(firstName.charAt(2)); // i
 console.log(firstName[2]); // i
 console.log(firstName.charCodeAt(2)); // 115 (Ascii Code)
+
+// Check Presence of Character
+console.log(firstName.includes("r")); // false (& if present it return true)
+console.log(firstName.indexOf("i")); // 2 (& if not present it return -1)
+console.log(firstName.lastIndexOf("i")); // 7
