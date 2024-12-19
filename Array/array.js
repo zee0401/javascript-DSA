2048e2455904223573a3e6a584c1c2319ec5dd87
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-19 14:25:16 +0530

    how to access the areay element

--- Array/array.js
+++ Array/array.js
@@ -2,3 +2,9 @@
 const arr = [1, 2, 3, 4, "Hello", { name: "Vishal" }, [1, 2, 3], 4];
 const arr2 = new Array("23", 223, "Zeshan");
 console.log(arr2);
+
+// How do you access the first and last elements of an array?
+const firstElement = arr[0]; // O(1)
+const arrLength = arr.length;
+const lastElement = arr[arrLength - 1];
+console.log(firstElement, arrLength, lastElement);
