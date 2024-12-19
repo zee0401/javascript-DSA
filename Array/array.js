f59ff455e0652be79bc266c4e09f1ef08c447ebf
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-19 14:24:20 +0530

    create a new array

--- Array/array.js
+++ Array/array.js
@@ -1,4 +1,4 @@
 // How do you create an empty array in JavaScript?
 const arr = [1, 2, 3, 4, "Hello", { name: "Vishal" }, [1, 2, 3], 4];
-// const arr2 = new Array();
-console.log(arr);
+const arr2 = new Array("23", 223, "Zeshan");
+console.log(arr2);
