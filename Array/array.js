b15e6a7e0bd28620772517d8258f810ae27b811a
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-19 16:30:33 +0530

    check if both array is equal

--- Array/array.js
+++ Array/array.js
@@ -38,3 +38,22 @@ for (let x of arr) {
 const newArr = [...arr, ...arrE];
 const newArr2 = arr.concat(arrE);
 console.log(newArr, newArr2);
+
+// How can you check if two arrays are equal?
+const isArrayEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+  // One Line solution
+  // return arr1.length === arr2.length && arr1.every((ele, i) => arr1[i] === arr2[i]);
+};
+
+console.log(isArrayEqual([1, 2, 3], [1, 2, 3]));
