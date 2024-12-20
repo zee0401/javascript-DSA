1af0ac79516c1a2f7601460a0ecc51568251840b
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-12-20 10:59:40 +0530

    Creating an object

---
+++ Objects/object.js
@@ -0,0 +1,16 @@
+// Creating an object
+const person = {
+  name: "Vishal",
+  age: 21,
+  isEducator: true,
+  skills: ["C++", "JavaScript", "ReactJS"],
+  projects: {
+    "Frontend Freaks": "Frontend Development Project",
+  },
+  code: function () {
+    return "start coding";
+  },
+  walk: () => {
+    return "start walking";
+  },
+};
